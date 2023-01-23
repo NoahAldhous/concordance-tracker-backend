34862912e683821fa00fea4a96ba6c3faa984a39
NoahAldhous <naldhous@hotmail.co.uk>
2023-01-23 14:18:01 +0000

    re-ordered code in index.js for better readability

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import express from 'express';
 import {people} from './data.js';
 
 const app = express();
+const PORT = 3001
 
+//Basic GET reqeust handling
 app.get('/', (request, response) => {
         response.json(people)
 });
 
-const PORT = 3001
-
+//App listening on PORT
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
